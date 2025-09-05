f6f94ed50c10571dd64d08f9851809ce5a4c49f8
IsmailAtaev <ataewisma@gmail.com>
2025-09-05 17:18:04 +0500

    feat: ecossis

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,11 @@
 module.exports = {
     apps: [
         {
-            name: 'api',
+            name: 'api-v2',
             script: './build/src/index.js',
             time: true,
             kill_timeout: 10000,
-            instances: 3,
+            instances: 1,
             exec_mode: 'cluster',
             env: {
                 NODE_ENV: 'production',
